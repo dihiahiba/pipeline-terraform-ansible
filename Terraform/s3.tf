0fcd5add05dfc8a062e4068829d4b03d14f75545
Mariam Yammoun <162291710+Mariamyammoun@users.noreply.github.com>
2024-10-06 20:06:53 +0100

    Update s3.tf

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -1,6 +1,6 @@
 # Création du bucket S3
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "mariam-bucket"  # Remplace par un nom unique pour ton bucket
+  bucket = "mariam-bucket" 
 
   tags = {
     Name = "MariamBucket"
