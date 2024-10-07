e3bae55f2848be06b72cdec5e2085c643bc8223b
Mariam Yammoun <162291710+Mariamyammoun@users.noreply.github.com>
2024-10-07 16:59:41 +0100

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -93,7 +93,7 @@ resource "aws_instance" "my_server" {
 }
 # Output pour récupérer l'adresse IP publique de l'instance EC2
 output "instance_public_ip" {
-  value = MariamServer.public_ip
+  value = aws_instance.my_server.public_ip
   description = "The public IP address of the EC2 instance"
 }
 
