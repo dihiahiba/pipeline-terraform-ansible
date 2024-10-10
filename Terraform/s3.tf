037e9548e99f1bbcc7a7ad5e6568a11477be5294
dihiahiba <dihiahiba@gmail.com>
2024-10-10 11:48:44 +0100

    Update s3.tf

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -1,8 +1,8 @@
 # Création du bucket S3
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "mariam-bucket" 
+  bucket = "hiba-bucket" 
 
   tags = {
-    Name = "MariamBucket"
+    Name = "HibaBucket"
   }
 }
